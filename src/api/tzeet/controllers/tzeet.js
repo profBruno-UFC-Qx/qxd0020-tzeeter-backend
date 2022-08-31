6b4e101d26e58592c99854e7cbb484bc9f39495a
Bruno Mateus <brunomateus@gmail.com>
2022-08-31 10:58:41 -0300

    Primeira versão do backend

---
+++ src/api/tzeet/controllers/tzeet.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  tzeet controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::tzeet.tzeet');
---
+++ src/api/tzeet/services/tzeet.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * tzeet service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::tzeet.tzeet');
