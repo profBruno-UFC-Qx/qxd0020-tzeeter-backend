5b4e90cd3a110d1b4fea2de455fceaa9fa4ae546
Bruno Mateus <brunomateus@gmail.com>
2024-12-05 14:15:19 -0300

    Atualizando o Strapi

--- config/plugins.js
+++ config/plugins.js
@@ -1,14 +1,2 @@
 module.exports = ({ env }) => ({
-  // ..
- 'transformer': {
-    enabled: true,
-    config: {
-      prefix: '/api/',
-      responseTransforms: {
-        removeAttributesKey: true,
-        removeDataKey: true,
-      }
-    }
-  },
-  // ..
 });
